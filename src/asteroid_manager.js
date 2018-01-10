9913463245b5581b502b9912df90d8936baebc30
MassKonFuzion <6656037+masskonfuzion@users.noreply.github.com>
2018-01-09 22:35:56 -0600

    Cleared some TODO items; updated bannedLocations (for asteroid spawning) to factor in all ships (human + AI)

--- src/asteroid_manager.js
+++ src/asteroid_manager.js
@@ -38,8 +38,7 @@ AsteroidManager.prototype.initialize = function(initAsteroids, maxAsteroids) {
 
     // Notes on bannedLocations:
     //  - We're probably taking references to the ship's (or ships') position(s), which is what we want
-    // TODO - for all bannedLocations assignments, use a list with all the ships in the shipList
-    var bannedLocations = [ {"position": this.parentObj.gameObjs["ship0"].components["physics"].currPos, "radius": 100 } ];
+    var bannedLocations = this.createBannedLocationsList(100);  // The parameter is the radius from each banned location, within which asteroids cannot be spawned
     for (var i = 0; i < initAsteroids; i++) {
         // Note the "funcCalls" property - "params" is a list that, when passed into a function.apply() call, is "splatted" into individual parameters, similar to Python *args
         var configObj = { "renderCompType": "image",
@@ -74,7 +73,7 @@ AsteroidManager.prototype.update = function(dt_s, config = null) {
         myEmitter.setPosition(spawnPos[0], spawnPos[1]);
         myEmitter.setVelocityRange(1.0, 5.0);   // TODO Confirm.. do I really need this here? I thought I only needed to set the velocity range one time, in the initialize function
 
-        var bannedLocations = [ {"position": this.parentObj.gameObjs["ship0"].components["physics"].currPos, "radius": 50 } ];
+        var bannedLocations = this.createBannedLocationsList(50);
         var configObj = { "renderCompType": "image",
                           "imageRef": game.imgMgr.imageMap["astLarge"].imgObj,
                           "funcCalls": [ {"func": Asteroid.prototype.setSize, "params": [2]} ],
@@ -91,12 +90,6 @@ AsteroidManager.prototype.update = function(dt_s, config = null) {
     }
 };
 
-
-// TODO either delete setAsteroidCounts() or keep it, and flesh it out
-//AsteroidManager.prototype.setAsteroidCounts = function(initAsteroids, maxAsteroids) {
-//    this.initAsteroids = initAsteroids;
-//    this.maxAsteroids = maxAsteroids;
-//};
 
 AsteroidManager.prototype.resetAsteroidField = function() {
     // TODO revisit what to do when you reset
@@ -148,7 +141,8 @@ AsteroidManager.prototype.disableAndSpawnAsteroids = function(params) {
         astToDisable.disable({"collisionMgrRef": this.components["asteroidPS"].collisionMgrRef});
         this.activeAsteroids[astToDisable.size] -= 1;
 
-        var bannedLocations = [ {"position": this.parentObj.gameObjs["ship0"].components["physics"].currPos, "radius": 50 } ];
+        //var bannedLocations = [ {"position": this.parentObj.gameObjs["ship0"].components["physics"].currPos, "radius": 50 } ];
+        var bannedLocations = this.createBannedLocationsList(50);
         // TODO trigger a particle explosion
         if (astToDisable.size > 0) {
             var newSize = astToDisable.size - 1;
@@ -196,4 +190,24 @@ AsteroidManager.prototype.executeCommand = function(cmdMsg, params) {
     // Call function
     // Note that this command passes a "params" arg in the cmdMsg payload, where other executeCommand functions (elsewhere in this codebase) do not..
     this.commandMap[cmdMsg].call(this, params); // use call() because without it, we're losing our "this" reference (going from AsteroidManager to Object)
+};
+
+
+// Create a list of banned locations, to be processed by asteroid spawning functions
+AsteroidManager.prototype.createBannedLocationsList = function(radius) {
+    var bannedLocations = [];
+
+    // iterate over the gameLogic object's shipDict to get the positions of ships in play
+    //for (var objID of Object.getOwnPropertyNames(this.parentObj.shipDict)) {  // The long-form way to iterate?
+    for (var objID in this.parentObj.shipDict) {
+        var shipID = this.parentObj.shipDict[objID];
+
+        var bannedLocItem = { "position": this.parentObj.gameObjs[shipID].components["physics"].currPos
+                            , "radius": radius
+                            };
+
+        bannedLocations.push(bannedLocItem);
+    };
+
+    return bannedLocations;
 };
