c6c3d0c9cc0830126f33468361e729b57a5af7b1
MassKonFuzion <6656037+masskonfuzion@users.noreply.github.com>
2018-02-26 18:25:07 -0600

    Corrected asteroid spawning to avoid ships

--- src/game_state_playing.js
+++ src/game_state_playing.js
@@ -42,13 +42,13 @@ GameStatePlaying.prototype.postRender = function(canvasContext, dt_s) {
         textPosY = 0.066667 + i * 0.05;
 
         deathsLabelPosNDC = [0.32, textPosY];      // NDCs go from 0 to 1 on each axis
-        deathsPosNDC = [0.40, textPosY];      // NDCs go from 0 to 1 on each axis    //TODO figure out right/left align (maybe canvas can do this for you?)
+        deathsPosNDC = [0.40, textPosY];      // NDCs go from 0 to 1 on each axis
 
         killsLabelPosNDC = [0.52, textPosY];      // NDCs go from 0 to 1 on each axis
-        killsPosNDC = [0.60, textPosY];      // NDCs go from 0 to 1 on each axis    //TODO figure out right/left align (maybe canvas can do this for you?)
+        killsPosNDC = [0.60, textPosY];      // NDCs go from 0 to 1 on each axis
 
         scoreLabelPosNDC = [0.72, textPosY];      // NDCs go from 0 to 1 on each axis
-        scorePosNDC = [0.80, textPosY];      // NDCs go from 0 to 1 on each axis    //TODO figure out right/left align (maybe canvas can do this for you?)
+        scorePosNDC = [0.80, textPosY];      // NDCs go from 0 to 1 on each axis
 
         canvasContext.font = "18px GameFont";  // Testing
         canvasContext.fillStyle = jankyListOfScoreColors[i];  // TODO Eventually:  change color to match player ship color (or, store it with a spaceship profile of some sort, which is enabled when the player selects a ship)
